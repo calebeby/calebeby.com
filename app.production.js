736294ab50ab7b87a76999a2f6d12ef025fd9020
Caleb Eby <caleb.eby01@gmail.com>
2017-05-06 14:09:03 -0700

    Remove uglifyjs so that production build works

--- app.production.js
+++ app.production.js
@@ -1,16 +1,16 @@
-const htmlStandards = require('reshape-standard');
-const cssStandards = require('spike-css-standards');
-const {UglifyJsPlugin} = require('webpack').optimize;
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const {UglifyJsPlugin} = require('webpack').optimize
 
 module.exports = {
   // Disable source maps
   devtool: false,
   // Minify js
-  plugins: [new UglifyJsPlugin()],
+  plugins: [],
   // Minify html and css
   reshape: htmlStandards({minify: true}),
   postcss: cssStandards({
     minify: true,
-    warnForDuplicates: false, // Cssnano includes autoprefixer
-  }),
-};
+    warnForDuplicates: false // Cssnano includes autoprefixer
+  })
+}
