ffbe660c1dd0336bf86d05cd09f174633f8b3dd6
Caleb Eby <caleb.eby01@gmail.com>
2017-05-28 13:59:16 -0700

    Fix production config

--- app.production.js
+++ app.production.js
@@ -1,3 +1,20 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const pageId = require('spike-page-id')
+
+const locals = {}
+
 module.exports = {
-  devtool: none
+  devtool: false,
+  reshape: htmlStandards({
+    minify: true,
+    locals: ctx => {
+      locals.pageId = pageId(ctx)
+      return locals
+    }
+  }),
+  postcss: cssStandards({
+    minify: true,
+    browsers: '> 1%'
+  })
 }
