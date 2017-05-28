fc205c531c320d307cdcaf2e656b4891740a3bc6
Caleb Eby <caleb.eby01@gmail.com>
2017-05-28 13:35:28 -0700

    Remove unused dependencies

--- app.production.js
+++ app.production.js
@@ -1,16 +1,3 @@
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const {UglifyJsPlugin} = require('webpack').optimize
-
 module.exports = {
-  // Disable source maps
-  devtool: false,
-  // Minify js
-  plugins: [],
-  // Minify html and css
-  reshape: htmlStandards({minify: true}),
-  postcss: cssStandards({
-    minify: true,
-    warnForDuplicates: false // Cssnano includes autoprefixer
-  })
+  devtool: none
 }
