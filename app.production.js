75d86a03330e014f7e339366b899cdb921d723e2
Caleb Eby <caleb.eby01@gmail.com>
2017-06-12 19:42:57 -0700

    Add resume

--- app.production.js
+++ app.production.js
@@ -1,20 +1,3 @@
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const pageId = require('spike-page-id')
-
-const locals = {}
-
 module.exports = {
-  devtool: false,
-  reshape: htmlStandards({
-    minify: true,
-    locals: ctx => {
-      locals.pageId = pageId(ctx)
-      return locals
-    }
-  }),
-  postcss: cssStandards({
-    minify: true,
-    browsers: '> 1%'
-  })
+  devtool: false
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,44 @@
-// Require('spike-pushstate');
-console.log('you can use ES6 here : )');
+const experiences = document.querySelector('.experiences > div')
+const experiencesList = document.querySelector('.experiences > div > ul')
+
+const leftButton = document.createElement('button')
+const rightButton = document.createElement('button')
+
+leftButton.innerHTML = '‹'
+rightButton.innerHTML = '›'
+
+experiences.appendChild(leftButton)
+experiences.appendChild(rightButton)
+
+const clickDistance = 150
+
+leftButton.addEventListener('click', () => {
+  experiencesList.scrollLeft -= clickDistance
+})
+
+rightButton.addEventListener('click', () => {
+  experiencesList.scrollLeft += clickDistance
+})
+
+const conditionallyShowScrollButtons = () => {
+  const left = experiencesList.scrollLeft
+  const right =
+    experiencesList.scrollWidth -
+    (experiencesList.scrollLeft + experiencesList.clientWidth) +
+    1
+
+  if (left === 0) {
+    leftButton.classList.add('hidden')
+  } else {
+    leftButton.classList.remove('hidden')
+  }
+  if (right === 1) {
+    rightButton.classList.add('hidden')
+  } else {
+    rightButton.classList.remove('hidden')
+  }
+}
+
+conditionallyShowScrollButtons()
+
+experiencesList.addEventListener('scroll', conditionallyShowScrollButtons)
